5109ad13c2972930ad95131477bce0f3fa869b5b
bissli <bissli@xyz.org>
2024-04-09 14:27:36 +0000

    migrate mime.types guesser to libb-util

--- src/mail/__init__.py
+++ src/mail/__init__.py
@@ -1,14 +1,1 @@
-import logging
-import pathlib
-
-import wrapt
-
-
-@wrapt.patch_function_wrapper('mimetypes', 'init')
-def patch_mimetypes_init(wrapped, instance, args, kwargs):
-    """Patch init known files"""
-    knownfiles = pathlib.Path(__file__).parent.absolute() / 'mime.types'
-    return wrapped([str(knownfiles)])
-
-
 from mail.mail import *
